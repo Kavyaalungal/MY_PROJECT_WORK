67cdd7fc8f9c340a4f7cc3babe3985e55f891582
Kavyaalungal <kavyaalungal691@gmail.com>
2024-07-05 15:16:11 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,15 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-// src/index.jsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './Store'; // Import the store
 import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+// Use createRoot to render your application
+const root = document.getElementById('root');
+createRoot(root).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
